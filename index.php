cedbb9d44a6bf9e5f01b897f0d7ff803334f7f0d
Stefán Atli Ágústsson <stefanag261@nemi.tskoli.is>
2016-09-20 11:56:28 +0000

    s!

--- index.php
+++ index.php
@@ -35,42 +35,6 @@ shuffle($input);
                         </div>
                 </form>
                     <p>New user?</p>
-                    <a data-toggle="modal" data-target="#myModal2">Sign up now!</a>
-
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Login</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-                <body id="page-top" class="index">
-        <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Sign in</h4>
-              </div>
-              <div class="modal-body">
-                
-                <form class="form-horizontal">  
-                        <div class="form-group">   
-                                <label for="inputEmail3" class="col-sm-2 control-label">Email </label>
-                                <div class="col-sm-10">   
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                                </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                            </div>
-                        </div>
-                </form>
-                    <p>New user?</p>
                     <a href="">Sign up now!</a>
 
               </div>
